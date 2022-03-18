a2105b7d39f8b14c4016b4101333bbf87ec8dcc3
Ganesh Gaikwad <47585923+ganeshgaikwadking@users.noreply.github.com>
2022-03-18 17:35:08 +0530

    BAnkDetails

---
+++ BankExample/Bankcl.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankExample
+{
+    public class Bankcl
+    {
+        public event Displaymsg1 LowBalace;
+        public event Displaymsg1 deposite;
+        public event Displaymsg1 ZeroBal;
+        public event Displaymsg1 InsufficeintBal;
+        double bal;
+
+        public Bankcl()
+        {
+            bal = 10000;
+
+        }
+        public double Deposit(double Amount)
+        {
+            bal += Amount;
+            return bal;
+        }
+        public double Withdraw(double Amount)
+        {
+
+            double available = bal - Amount;
+            if(available<0)
+            {
+                InsufficeintBal();
+                return bal;
+            }
+            else
+            {
+                bal -= Amount;
+                if(bal==0)
+                {
+                    ZeroBal();
+                    return bal;
+
+                }
+                else if(bal<3000)
+                {
+                    LowBalace();
+                    return bal;
+                }
+                else
+                {
+                    return bal;
+                }
+            }
+        }
+
+    }
+    
+}
---
+++ BankExample/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankExample
+{
+    public delegate void Displaymsg1();
+     class Program
+    {
+        public void Deposite()
+        {
+            Console.WriteLine("deposited Amount...");
+        }
+        static void LowBalane()
+        {
+            Console.WriteLine("Balance Is Low");
+        }
+        static void ZeroBal()
+        {
+            Console.WriteLine("Your Balance Is Zero....");
+        }
+        static void InsufficientBalance()
+        {
+            Console.WriteLine("Yuor Balance Is Insufficient....");
+        }
+       
+
+        static void Main(string[] args)
+        {
+            Bankcl bank = new Bankcl();
+            bank.LowBalace +=new  Displaymsg1(LowBalane);
+            bank.ZeroBal += new Displaymsg1(ZeroBal);
+            bank.InsufficeintBal+=new Displaymsg1(InsufficientBalance);
+            
+            double bal = bank.Withdraw(12000);
+            Console.WriteLine("your Balance Is " + bal);
+        }
+    }
+}
